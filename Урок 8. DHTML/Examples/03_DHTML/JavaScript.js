465b150ff8c3f712cb11bcc6ab5836098768c691
oleksandrpetryk <o.petryk@dinamicka.com>
2015-07-14 23:09:03 +0300

    Lessons8

---
+++ Урок 8. DHTML/Examples/03_DHTML/JavaScript.js
@@ -0,0 +1,12 @@
+﻿var div1;
+var leftPos = 0;
+
+window.onload = function () {
+  div1 = document.getElementById("div1");
+  setInterval(animate, 10);
+}
+
+function animate() {
+  div1.style.left = leftPos + "%";
+  leftPos++;
+}
