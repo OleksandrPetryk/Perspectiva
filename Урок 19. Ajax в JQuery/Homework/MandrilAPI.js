5070e28e6254e776321d5537401c0a92cddeb60d
Oleksandr Petryk <o.petryk@dinamicka.com>
2015-12-24 21:07:57 +0200

    mandrillAPI

---
+++ Урок 19. Ajax в JQuery/Homework/MandrilAPI.js
@@ -0,0 +1,16 @@
+﻿Data = JSON.stringify({
+    'key': 'V0D_Zxz9tADoT1PJUBYXhQ', // Get from mandrillapp.com 
+    'message': {
+        'from_email': 'yuor@gmail.com',
+        'to': [{ 'email': "sale@gmail.com", 'type': 'to' }],
+        'autotext': 'true',
+        'subject': 'New order',
+        'html': ' <table style=""> <tr> <td>' + input1 + '</td> </tr> </table> '
+    }
+})
+
+$.ajax({
+    type: "POST",
+    url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+    data: Data,
+});
